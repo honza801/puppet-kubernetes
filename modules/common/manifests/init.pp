063c3061d340a72a5ecae4b3af0ae4781bbbb181
Jan Krcmar <honza801@gmail.com>
2023-05-30 09:17:10 +0200

    class common file /root/.vimrc

--- modules/common/manifests/init.pp
+++ modules/common/manifests/init.pp
@@ -1,6 +1,12 @@
 class common {
+    File {
+        owner => 'root', group => 'root', mode => '0644',
+    }
+
     file { '/root/.bash_aliases':
         source => 'puppet:///modules/common/bash_aliases',
-        owner => 'root', group => 'root', mode => '0644',
+    }
+    file { '/root/.vimrc':
+        source => 'puppet:///modules/common/vimrc',
     }
 }
